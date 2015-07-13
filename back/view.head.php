e8f51e93504b3acd080da2f9592a1964d4720066
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-07-13 16:18:17 +0200

    share in github ribbon

--- back/view.head.php
+++ back/view.head.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="<?= ROOT_PATH . '/bower_components/materialize/dist/css/materialize.min.css' ?>"/>
+    <link rel="stylesheet" href="<?= ROOT_PATH . '/bower_components/github-fork-ribbon-css/gh-fork-ribbon.css' ?>"/>
     <style>
         .collection .collection-item {
             padding-left: 40px;    
